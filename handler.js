75bed09d0f9d3365565499b63d3e9f60eed89258
spff <spff@bitmessage.ch>
2019-08-16 09:25:13 +0800

    Impl handler, untract dist

--- handler.js
+++ handler.js
@@ -5,68 +5,150 @@ AWS.config.update({
   secretAccessKey: process.env.secretAccessKey
 });
 
-const dynamodb = new AWS.DynamoDB();
+let ddb = null
+const tableName = 'exlentflow'
 const router = {}
 
 import axios from 'axios'
+const uuidv4 = require('uuid/v4')
 
 async function getSession(body) {
-  
-  return await axios.post('https://auth.exlent.io/api/auth/get_session', {'session': body.session})
-  .catch(err=> {
-    return err.response
-  })
+
+  return await axios.post('https://auth.exlent.io/api/auth/get_session', { 'session': body.session })
+    .catch(err => {
+      return err.response
+    })
 }
 
 const handler = async (event) => {
   // TODO implement
   if (event.httpMethod != "POST") {
-      return { statusCode: 404, body: 'POST only' }
+    return { statusCode: 404, body: 'POST only' }
   }
   if (!router.hasOwnProperty(event.path)) {
-      return { statusCode: 404, body: 'Endpoint not found' }
+    return { statusCode: 404, body: 'Endpoint not found' }
   }
-  
+
   const bd = JSON.parse(event.body)
-  
+
   if (!bd.hasOwnProperty('session')) {
-      return { statusCode: 400, body: 'missing key: session'}
+    return { statusCode: 400, body: 'missing key: session' }
   }
   const auth = await getSession(bd)
-  
+
   if (auth.status != 200) {
-      return { statusCode: 401, body: '' }
+    return { statusCode: 401, body: '' }
   }
+  ddb = new AWS.DynamoDB.DocumentClient()
+
+  bd.auth = auth.data
   return router[event.path](bd)
-  
+
 
 };
 
-router['/create-flow'] = function(event) {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(event),
+router['/create-flow'] = async function (event) {
+  if (event.n == null) {
+    return { statusCode: 400, body: 'missing key' }
+  }
+
+  const flowid = uuidv4().replace(/-/g, '')
+  try {
+    const r = await ddb.put({
+      TableName: tableName,
+      Item: {
+        'gid': event.auth.gid,
+        'flowid': flowid,
+        // name
+        'n': event.n
+        // do not put empty array or empty string with doc lib or exception occurs
+        // data source
+        // 'ds': [],
+        // template
+        // 'tp': []
+      }
+    }).promise()
+    return { statusCode: 200, body: JSON.stringify({ 'flowid': flowid }) }
+  } catch (error) {
+    return { statusCode: 500, body: `write ddb err: ${error.stack}` }
+  }
+};
+
+router['/list-flows'] = async function (event) {
+  let r = null
+  try {
+    r = await ddb.query({
+      TableName: tableName,
+      KeyConditionExpression: "gid = :gid",
+      ExpressionAttributeValues: {
+        ":gid": event.auth.gid
+      }
+    }).promise()
+    return { statusCode: 200, body: JSON.stringify(r) }
+  } catch (error) {
+    return { statusCode: 500, body: `query ddb err: ${error.stack}` }
+  }
+};
+
+
+router['/get-flow'] = async function (event) {
+  if (event.n == null) {
+    return { statusCode: 400, body: 'missing key' }
+  }
+
+  let r = null
+  try {
+    r = await ddb.get({
+      TableName: tableName,
+      Key: {
+        "gid": gid,
+        "n": event.n
+      }
+    }).promise()
+    return { statusCode: 200, body: JSON.stringify(r) }
+  } catch (error) {
+    return { statusCode: 500, body: `query ddb err: ${error.stack}` }
+  }
+};
+
+router['/update-flow'] = async function (event) {
+  for (key in ['n']) {
+    if (event[key] == null) {
+      return { statusCode: 400, body: 'missing key' }
+    }
+  }
+
+  let exp = "set"
+  const expAttr = {}
+  for (key in ['ds','tp']) {
+    if (event[key] != null) {
+      exp += ` ${key} = :${key}`
+      expAttr[`:${key}`] = event[key]
+    }
+  }
+
+  var params = {
+    TableName: tableName,
+    Key:{
+        "gid": event.auth.gid,
+        "n": event.n
+    },
+    UpdateExpression: "set info.rating = :r, info.plot=:p, info.actors=:a",
+    ExpressionAttributeValues:{
+        ":r":5.5,
+        ":p":"Everything happens all at once.",
+        ":a":["Larry", "Moe", "Curly"]
+    },
+    ReturnValues:"UPDATED_NEW"
   };
-  return response;
+
+  console.log("Updating the item...");
+  ddb.update(params, function(err, data) {
+
+  return { statusCode: 200, body: JSON.stringify(event) }
 };
 
-router['/get-flows'] = function(event) {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(event),
-  };
-  return response;
-};
-
-router['/update-flow'] = function(event) {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(event),
-  };
-  return response;
-};
-
-router['/delete-flow'] = function(event) {
+router['/delete-flow'] = async function (event) {
   const response = {
     statusCode: 200,
     body: JSON.stringify(event),
